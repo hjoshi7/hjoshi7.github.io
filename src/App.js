49a8dea82a5f8c8c0c80aeb5ec766faa67b762ea
hjoshi7 <hrishijoshi5@gmail.com>
2022-11-08 17:12:46 -0800

    my commit

--- src/App.js
+++ src/App.js
@@ -4,16 +4,23 @@ import Home from './pages/Home'
 import Translator from './pages/Translator'
 import Background from './pages/Background'
 import Practice from './pages/Practice'
+import React from 'react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/translator" element={<Translator />}/>
-        <Route path="/practice" element={<Practice />}/>
-      </Routes>
-    </Router>
+    <React.StrictMode>
+    <ChakraProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/translator" element={<Translator />}/>
+          <Route path="/practice" element={<Practice />}/>
+        </Routes>
+      </Router>
+    </ChakraProvider>
+  </React.StrictMode>
+  
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './globals.css'
+import { render } from "react-dom";
 
-import { ChakraProvider, extendTheme } from '@chakra-ui/react'
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <App />
+// );
+const rootElement = document.getElementById("root");
+render(<App />, rootElement);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
